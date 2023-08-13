05d0402ee3eca6bb65c110e1157cd6a6f205d0f0
sinan <sinan@sinans-MacBook-Air.local>
2023-08-13 19:11:35 +0530

    updated the backend URL's

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -5,5 +5,5 @@ export const axiosTeacherInstance = axios.create({baseURL :"https://etrain-z30o.
 export const axiosPrincipalInstance = axios.create({baseURL :"https://etrain-z30o.onrender.com/principal"})
 // export const axiosStudentInstance = axios.create({baseURL :"http://localhost:4000"})
 // export const axiosTeacherInstance = axios.create({baseURL :"http://localhost:4000/teacher"})
-// export const axiosPrincipalInstance = axios.create({baseURL :"http://localhost:4000/principal"})
+// export const axiosPrincipalInstance = axios.create({baseURL :"http://localhost:4000/principal"}) 
 
